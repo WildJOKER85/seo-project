86bcfdf8b386d115aa05c4ea2e30bcfb6e859c2f
Your Name <morbius071585@gmail.com>
2024-02-12 00:00:32 +0400

    react-project

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./styles/App.css";
 // import { SidebarItem } from "./components/SidebarItem";
 // import Header from "./components/Body";
 // import SideColumn from "./components/SideColumn";
-// import MainProject from "./components/MainProject";
-import Test from "./components/Test";
+import MainProject from "./components/MainProject";
+// import Test from "./components/Test";
 
 function App() {
 
@@ -12,8 +12,8 @@ function App() {
       {/* <SidebarItem />
       <Header />
       <SideColumn /> */}
-      {/* <MainProject /> */}
-      <Test />
+      <MainProject />
+      {/* <Test /> */}
     </div>
   );
 }
