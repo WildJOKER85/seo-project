b4f1e0374a7fe2362035bce2feb391ce7e4433c9
Your Name <morbius071585@gmail.com>
2024-02-14 04:47:41 +0400

    added Aside and RightBar

--- src/components/SeoProject/Aside/index.jsx
+++ src/components/SeoProject/Aside/index.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import "./style.css";
+import presentSeo from "../../../assets/14-magnta-trade.jpg";
+import texrobot from "../../../assets/texno-robot.jpeg";
 
 const Aside = () => {
     return (
         <div className="wrapperAside">
+            <div className="asideMedia">
+                <img src={presentSeo} alt="" />
+            </div>
 
+            <p>День рождения SEOnews: нам 14 <br />лет!</p>
+
+            <div className="asideFutter">
+                <div className="mediaRobot">
+                    <img src={texrobot} alt="" />
+                </div>
+                <div className="futterAsideTitle">
+                    <p id="firstAsideLine">Новости</p>
+                    <p id="secondAsideLine">Технодайджест: летающие <br />автомобили и беспилотные <br />
+                        автобусы
+                    </p>
+                    <p id="threeLine">Кажется, мы уже в будущем</p>
+                    <div className="futterLine">
+                        <span>2 дня назад</span>
+                        <ion-icon name="eye-outline"></ion-icon> <span>3066</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
+
 
 export default Aside;
--- src/components/SeoProject/RightBar/index.jsx
+++ src/components/SeoProject/RightBar/index.jsx
@@ -1,9 +1,52 @@
 import React from "react";
 import "./style.css";
+import persIcon from "../../../assets/darya.jpg";
+import persIcon2 from "../../../assets/anna.jpg";
+import persIcon3 from "../../../assets/uifaces-alex.jpg";
 
 const RightBar = () => {
     return (
         <div className="wrapperRightBar">
+            <h1>Прямой Эфир</h1>
+            <div className="personInfo">
+                <img src={persIcon} alt="" />
+                <div className="personName">
+                    <p>Дарья Калинская</p>
+                    <p>42 минуты назад</p>
+                </div>
+            </div>
+            <p>Baltic Digital Days 2019: объявлены <br /> лауреаты премии за достижения в <br />
+                интернет-маркентинге
+            </p>
+            <div className="shadowStyle">
+                <p>Александр спасибо :) и за <br /> поздравление, и за поддержку!</p>
+            </div>
+
+
+            <div className="personInfo">
+                <img src={persIcon2} alt="" />
+                <div className="personName">
+                    <p>Анна Макарова</p>
+                    <p>42 минуты назад</p>
+                </div>
+            </div>
+            <p id="balticDigital">Baltic Digital Days 2019: объявлены <br /> лауреаты премии за достижения в <br />
+                интернет-маркентинге
+            </p>
+            <div className="shadowStyle">
+                <p>Александр спасибо :) Очень <br /> приятно! </p>
+            </div>
+
+            <div className="personInfo">
+                <img src={persIcon3} alt="" />
+                <div className="personName">
+                    <p>Александр Сопоев</p>
+                    <p>50 минуты назад</p>
+                </div>
+            </div>
+            <p id="balticDigital">Baltic Digital Days 2019: объявлены <br /> лауреаты премии за достижения в <br />
+                интернет-маркентинге
+            </p>
 
         </div>
     )
